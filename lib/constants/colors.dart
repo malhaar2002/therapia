a58ec52f880a022e92a30b5584344412a1782b3e
malhaar2002 <aroramalhaar@gmail.com>
2022-09-17 13:45:23 +0530

    Made structure of dashboard screen

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/painting.dart';
+
+const Color apnaOrange = Color(0xFFF76E11);
+const Color apnaYellow = Color(0xFFFF9F45);
+const Color apnaSkin = Color(0xFFFFBC80);
+const Color apnaRed = Color(0xFFFC4F4F); 
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:therapia/constants/colors.dart';
 
 class Dashboard extends StatelessWidget {
-  const Dashboard ({super.key});
+  const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: apnaRed,
+        child: const Icon(Icons.add),
+      ),
+      backgroundColor: apnaSkin,
+      appBar: AppBar(
+        backgroundColor: apnaRed,
+        title: const Text('Therapia'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: apnaYellow,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25)),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

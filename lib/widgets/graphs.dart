3265fb6dbf84e83c737357a52ca47c0df0cb2e12
malhaar2002 <aroramalhaar@gmail.com>
2022-09-17 19:42:32 +0530

    Changed to fl_graph

--- lib/widgets/graphs.dart
+++ lib/widgets/graphs.dart
@@ -1,36 +1,21 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:therapia/constants/colors.dart';
-import 'package:therapia/models/graph_model.dart';
 
 class Graphs extends StatefulWidget {
-  const Graphs({super.key});
+  const Graphs({Key? key}) : super(key: key);
 
   @override
-  // ignore: no_logic_in_create_state
-  State<Graphs> createState() =>
-      _GraphsState(timeDuration: GraphModel.monthData);
+  _LineChartSample2State createState() => _LineChartSample2State();
 }
 
-class _GraphsState extends State<Graphs> {
-  List<GraphModel> timeDuration;
-  _GraphsState({required this.timeDuration});
+class _LineChartSample2State extends State<Graphs> {
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
 
-  List<charts.Series<GraphModel, num>> series = [];
-
-  @override
-  void initState() {
-    super.initState();
-    series = [
-      charts.Series(
-        id: 'Weekly Graph',
-        data: timeDuration,
-        domainFn: (GraphModel series, _) => series.day,
-        measureFn: (GraphModel series, _) => series.tremorIndex,
-        colorFn: (_, __) => charts.ColorUtil.fromDartColor(apnaDark),
-      )
-    ];
-  }
+  bool showAvg = false;
 
   @override
   Widget build(BuildContext context) {
@@ -45,9 +30,7 @@ class _GraphsState extends State<Graphs> {
               'Week',
               style: TextStyle(color: Colors.white),
             ),
-            onPressed: () => setState(() {
-              timeDuration = GraphModel.weekData;
-            }),
+            onPressed: () => setState(() {}),
           ),
           TextButton(
             child: const Text(
@@ -56,43 +39,255 @@ class _GraphsState extends State<Graphs> {
                 color: Colors.white,
               ),
             ),
-            onPressed: () => setState(() {
-              timeDuration = GraphModel.monthData;
-            }),
+            onPressed: () => setState(() {}),
           ),
         ],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(25),
-              bottomRight: Radius.circular(25)),
-          color: apnaLight,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25)
+              ),
+              color: apnaLight,
+            ),
+            child: LineChart(
+              showAvg ? avgData() : mainData(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff68737d),
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 2:
+        text = const Text('MAR', style: style);
+        break;
+      case 5:
+        text = const Text('JUN', style: style);
+        break;
+      case 8:
+        text = const Text('SEP', style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 8.0,
+      child: text,
+    );
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff67727d),
+      fontWeight: FontWeight.bold,
+      fontSize: 15,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 1:
+        text = '10K';
+        break;
+      case 3:
+        text = '30k';
+        break;
+      case 5:
+        text = '50k';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text, style: style, textAlign: TextAlign.left);
+  }
+
+  LineChartData mainData() {
+    return LineChartData(
+      gridData: FlGridData(
+        show: false,
+        drawVerticalLine: false,
+        horizontalInterval: 1,
+        verticalInterval: 1,
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
         ),
-        width: MediaQuery.of(context).size.width,
-        child: charts.LineChart(
-          series,
-          defaultRenderer: charts.LineRendererConfig(
-            includeArea: true,
-            stacked: true,
-            strokeWidthPx: 3,
+        topTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: false,
+            reservedSize: 30,
+            interval: 1,
+            getTitlesWidget: bottomTitleWidgets,
           ),
-          animate: true,
-          domainAxis: const charts.NumericAxisSpec(
-            showAxisLine: false,
-            renderSpec: charts.NoneRenderSpec(),
-          ),
-          primaryMeasureAxis: const charts.NumericAxisSpec(
-            renderSpec: charts.NoneRenderSpec(),
-          ),
-          layoutConfig: charts.LayoutConfig(
-            leftMarginSpec: charts.MarginSpec.fixedPixel(0),
-            rightMarginSpec: charts.MarginSpec.fixedPixel(0),
-            topMarginSpec: charts.MarginSpec.fixedPixel(0),
-            bottomMarginSpec: charts.MarginSpec.fixedPixel(0),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: false,
+            interval: 1,
+            getTitlesWidget: leftTitleWidgets,
+            reservedSize: 42,
           ),
         ),
       ),
+      borderData: FlBorderData(show: false),
+      minX: 0,
+      maxX: 11,
+      minY: 0,
+      maxY: 6,
+      lineBarsData: [
+        LineChartBarData(
+          spots: const [
+            FlSpot(0, 3),
+            FlSpot(2.6, 2),
+            FlSpot(4.9, 5),
+            FlSpot(6.8, 3.1),
+            FlSpot(8, 4),
+            FlSpot(9.5, 3),
+            FlSpot(11, 4),
+          ],
+          isCurved: true,
+          gradient: LinearGradient(
+            colors: gradientColors,
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.3))
+                  .toList(),
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  LineChartData avgData() {
+    return LineChartData(
+      lineTouchData: LineTouchData(enabled: false),
+      gridData: FlGridData(
+        show: false,
+        drawHorizontalLine: false,
+        verticalInterval: 1,
+        horizontalInterval: 1,
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: false,
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: false,
+            reservedSize: 30,
+            getTitlesWidget: bottomTitleWidgets,
+            interval: 1,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: false,
+            getTitlesWidget: leftTitleWidgets,
+            reservedSize: 42,
+            interval: 1,
+          ),
+        ),
+        topTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        rightTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+      ),
+      borderData: FlBorderData(
+          show: false,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: 11,
+      minY: 0,
+      maxY: 6,
+      lineBarsData: [
+        LineChartBarData(
+          spots: const [
+            FlSpot(0, 3.44),
+            FlSpot(2.6, 3.44),
+            FlSpot(4.9, 3.44),
+            FlSpot(6.8, 3.44),
+            FlSpot(8, 3.44),
+            FlSpot(9.5, 3.44),
+            FlSpot(11, 3.44),
+          ],
+          isCurved: true,
+          gradient: LinearGradient(
+            colors: [
+              ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                  .lerp(0.2)!,
+              ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                  .lerp(0.2)!,
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: [
+                ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                    .lerp(0.2)!
+                    .withOpacity(0.1),
+                ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                    .lerp(0.2)!
+                    .withOpacity(0.1),
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
